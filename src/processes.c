195159f4e086f63b4455ee775cba696de8cd6d14
tatex74 <63496987+tatex74@users.noreply.github.com>
2023-12-18 12:17:02 +0100

    Update processes.c

--- src/processes.c
+++ src/processes.c
@@ -16,12 +16,6 @@
  * @return 0 if all went good, -1 else
  */
 int prepare(configuration_t *the_config, process_context_t *p_context) {
-    if (the_config == NULL || p_context == NULL){
-        return -1;
-    }
-    if (the_config->is_parallel){
-        
-    }
 }
 
 /*!
